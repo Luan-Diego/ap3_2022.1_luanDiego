76f7cdd304fb629f4bcfe9d3d6c2acebfe2442db
Luan <luan-diego10@hotmail.com>
2022-08-13 17:02:01 -0300

    Criação de classes Ap3 1.0

---
+++ Pessoa.java
@@ -0,0 +1,28 @@
+package com.br.ap3.luandiego;
+
+public class Pessoa{
+    private String nome;
+    private int idade;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome da Pessoa: "+this.nome+"/n"+
+                "Idade da Pessoa: "+this.idade;
+    }
+}
---
+++ TestePessoa.java
@@ -0,0 +1,7 @@
+package com.br.ap3.luandiego;
+
+public class TestePessoa {
+    public static void main(String[] args) {
+        System.out.println("Ola, java");
+    }
+}
