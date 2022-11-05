449104acdeaa68bb45282d1f8b862a78eba0f4f3
Luan <luan-diego10@hotmail.com>
2022-11-05 03:26:00 -0300

    Atividade 7 unidade 2

---
+++ Unidade2/br/com/luandiego/ap3/exercicio7/A1_TudoMaiusculo.java
@@ -0,0 +1,11 @@
+package Unidade2.br.com.luandiego.ap3.exercicio7;
+
+public class A1_TudoMaiusculo { 
+    public static void main(String args[]) {
+        String frase = null;
+        String novaFrase = null;
+        novaFrase = frase.toUpperCase();
+        System.out.println("Frase antiga: " + frase);
+        System.out.println("Frase nova: " + novaFrase);
+      }
+}
---
+++ Unidade2/br/com/luandiego/ap3/exercicio7/Contato.java
@@ -0,0 +1,5 @@
+package Unidade2.br.com.luandiego.ap3.exercicio7;
+
+public class Contato {
+    
+}
